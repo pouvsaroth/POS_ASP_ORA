95e1244d3356a83109f0ed26456f1ff9f77faf25
LinNavin123 <linnavin1235@gmail.com>
2026-02-25 19:27:37 +0700

    commit all

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,6 +10,8 @@ namespace POS_ASP_ORA.Data
 
         public DbSet<Users> UsersModel { get; set; }
 
+        public DbSet<Customer> CustomerModel { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Users>(entity =>
@@ -30,6 +32,39 @@ namespace POS_ASP_ORA.Data
                     .HasColumnType("NUMBER(1)");
                 entity.Property(e => e.CreatedAt)
                     .HasDefaultValueSql("SYSDATE");
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                      .HasColumnType("NUMBER");
+
+                entity.Property(e => e.CustomerName)
+                      .HasColumnType("VARCHAR2(50)")
+                      .IsRequired();
+
+                entity.Property(e => e.Sex)
+                      .HasColumnType("VARCHAR2(10)")
+                      .IsRequired();
+
+                entity.Property(e => e.Phone)
+                      .HasColumnType("VARCHAR2(50)")
+                      .IsRequired();
+
+                entity.Property(e => e.Email)
+                      .HasColumnType("VARCHAR2(100)")
+                      .IsRequired();
+
+                entity.Property(e => e.Address)
+                      .HasColumnType("VARCHAR2(150)")
+                      .IsRequired();
+
+
+                entity.Property(e => e.UserAccessId)
+                      .HasColumnType("RAW(16)");
+
             });
         }
     }
--- Migrations/20260225122143_createCustomerTblkk.Designer.cs
+++ Migrations/20260225122143_createCustomerTblkk.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 using POS_ASP_ORA.Data;
@@ -11,9 +12,11 @@ using POS_ASP_ORA.Data;
 namespace POS_ASP_ORA.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225122143_createCustomerTblkk")]
+    partial class createCustomerTblkk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
