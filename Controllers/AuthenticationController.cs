56eaa592bc2ddda8189033a05d3b496c1be189d8
NT-Saroth <pouvsaroth@gmail.com>
2026-03-05 17:04:53 +0700

    commit

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace POS_ASP_ORA.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        // POST: Login logic
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            // 🔥 Replace this with Oracle DB later
+            if (username == "admin" && password == "123")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+
+                var identity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            // TODO: Clear session or cookie
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
