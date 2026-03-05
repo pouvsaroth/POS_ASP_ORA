418d228e214e9ec130e659d2e7f14f24d7c85104
NT-Saroth <pouvsaroth@gmail.com>
2026-03-05 17:28:48 +0700

    COMMIT

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POS_ASP_ORA.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
