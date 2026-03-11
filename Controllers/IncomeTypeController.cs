ad13a4d6c56624de56f7595080067ed944170bbb
LinNavin123 <linnavin1235@gmail.com>
2026-03-11 19:01:39 +0700

    commmit

---
+++ Controllers/IncomeTypeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POS_ASP_ORA.Data;
+using POS_ASP_ORA.Models;
+
+namespace POS_ASP_ORA.Controllers
+{
+    public class IncomeTypeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public IncomeTypeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+       
+        public IActionResult IncomeType()
+        {
+            var incometypeData = _context.IncomeTypeModel.ToList();
+            return View("~/Views/Accounts/IncomeType.cshtml", incometypeData);
+        }
+
+        [HttpPost]
+        public IActionResult Create(IncomeType model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.IncomeTypeModel.Add(model);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("IncomeType");
+        }
+
+    }
+}
